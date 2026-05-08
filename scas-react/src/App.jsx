e4bed1b1553b9e2ea34824d053e7ce46b63e4729
kpvidhyudh <kpvidhyudh4@gmail.com>
2026-05-08 15:15:09 +0530

    initial commit

---
+++ scas-react/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Home from './Components/Home'
+import Counter from './Components/counter'
+
+const App = () => {
+  return (<Counter />);
+  
+}
+
+export default App
---
+++ scas-react/src/Components/Counter.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+const Counter = () => {
+    const [count, setCount] = useState(0);
+    const increase = () => {
+        setCount(count + 1);
+    };
+    return (
+        <>
+            <h1>{count}</h1>
+            <button onClick={increase}>Count</button>
+        </>
+    );
+};
+export default Counter;
